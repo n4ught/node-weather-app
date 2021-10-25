4412aa5e399e2bf492eeeeb978becb98fe249c9f
Arjay <arjay.otis@gmail.com>
2021-10-25 15:33:28 +0800

    Added weather text.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -17,7 +17,7 @@ const forecast = (longitude, latitude, callback) => {
 
         }else{
 
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out.");
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out. The humidity is: " + body.current.humidity  );
 
         }
 
